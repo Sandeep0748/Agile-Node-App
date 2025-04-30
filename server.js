7ee40e5ab86ca30f943218c911583a8ec9447f5c
Sandeep Kumar Yadav <sandeepkyadav017@gmail.com>
2025-04-30 13:07:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,4 +6,20 @@ app.get("/", (req, res) => {
     res.status(200).json({name: "agile-node-app"})
 });
 
+app.get("/Student",(req,res) =>{
+    res.status(200).json([
+        {
+        name: "Sandeep",
+        age: 24,
+        city: "gorakhpur"
+        },
+        {
+            name: "Akshay",
+            age: 24,
+            city: "gorakhpur"
+            }
+        
+    ])
+})
+
 app.listen(3000);
